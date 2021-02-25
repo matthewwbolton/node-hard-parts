b43d8386383478181243c76918e2ae6f32a2ac86
Matthew Bolton <matthewwbolton@gmail.com>
2021-02-25 15:44:54 -0500

    completed section of course entitled node with http - moving on to next section of the course on Error Handling

--- server.js
+++ server.js
@@ -1,32 +1,25 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-function doOnRequest(request, response){
+function doOnRequest(request, response) {
   // Send back a message saying "Welcome to Twitter"
   // code here...
-  response.end("yo")  
-  if (request.method === 'GET' && request.url === '/') {
+  response.end("yo");
+  if (request.method === "GET" && request.url === "/") {
     // read the index.html file and send it back to the client
     // code here...
-    fs.createReadStream()
-
-  }
-  else if (request.method === 'POST' && request.url === '/sayHi') {
+    fs.createReadStream();
+  } else if (request.method === "POST" && request.url === "/sayHi") {
     // code here...
-    
-  }
-  else if (request.method === 'POST' && request.url === '/greeting') {
+  } else if (request.method === "POST" && request.url === "/greeting") {
     // accumulate the request body in a series of chunks
     // code here...
-    
-  }
-  else {
+  } else {
     // Handle 404 error: page not found
     // code here...
-    
   }
 }
 
-const server = http.createServer(doOnRequest)
+const server = http.createServer(doOnRequest);
 
 server.listen(3000);
